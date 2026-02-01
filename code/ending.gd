cf6a177d296ea04040904e8e147ded48dab40857
Iris <iris@raidev.eu>
2026-02-01 15:37:00 +0200

    added exports

--- code/ending.gd
+++ code/ending.gd
@@ -5,10 +5,15 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body == Game.Player:
+		AudioServer.set_bus_volume_linear(1, 0)
 		Game.Player.state = "Stop"
 		Game.Camera.active = false
 		var t = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 		t.tween_property(Game.Camera, "position:x", $Jester.global_position.x, 2)
 		$CanvasLayer.show()
 		$AnimationPlayer.play("ending")
+		await  $AnimationPlayer.animation_finished
+		while not Input.is_action_pressed("ui_accept"):
+			await get_tree().process_frame
+		get_tree().change_scene_to_file("res://scene/Tutorial.tscn")
 	
